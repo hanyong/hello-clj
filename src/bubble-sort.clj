0488a66f5bc1cc8854de4255491d0e315ed23488
hanyong <313982441@qq.com>
2013-04-25 21:11:32 +0800

    修正冒泡排序运行异常

--- src/bubble-sort.clj
+++ src/bubble-sort.clj
@@ -2,47 +2,44 @@
 
 ; 一趟冒泡, 最大元素移动到最后
 (declare bubble-up-do)
-(defn bubble-up [coll] (do
-  (println (list 'bubble-up coll)) ; debug
-  ; 打散参数, 使能够按元素个数重载
-  (apply bubble-up-do coll)
-  ))
+(defn bubble-up [coll] 
+  (do
+    '(println ['bubble-up coll]) ; debug
+    ; 打散参数, 使能够按元素个数重载
+    (apply bubble-up-do coll)
+    ))
 (defn bubble-up-do
-  ([x] [x])
+  ([x] (do '(println ['bubble-up-do :one x]) [x]))
   ([x y & more]
-    (->>
+    (do 
+      '(println ['bubble-up-do :more x y more])
       ; 交换表头
-      (if (< x y)
-        (concat [x y] more)
-        (concat [y x] more)
-        )
-      ; 分离表头, 递归对表尾进行冒泡
-      ((fn [[x & more]]
-         (concat [x] (bubble-up more))
-         ))
-      )
-    )
-  )
+      (let [[a b] (if (< x y) [x y] [y x])]
+        (concat [a] (bubble-up (concat [b] more)))
+        ))))
 
 ; 冒泡排序
 (declare bubble-sort-do)
 (defn bubble-sort [coll]
-  ; 打散参数, 按元素个数重载
-  (apply bubble-sort-do coll)
-  )
+  (do
+    '(println ['bubble-sort coll])
+    ; 打散参数, 按元素个数重载
+    (apply bubble-sort-do coll)
+    ))
 (defn bubble-sort-do
   ([] [])
-  ([x] [x])
-  ([x & more] (->
-    (concat [x] more)
-    (bubble-up)
-    ; 分离最后一个元素，递归排序前面的元素
-    (#(concat
-      (bubble-sort (drop-last %))
-      (last %)
-      ))
-    ))
-  )
+  ([x] (do '(println ['bubble-sort-do :one x]) [x]))
+  ([x & more]
+    (do
+      '(println ['bubble-sort-do :more x more])
+      (->
+        (concat [x] more)
+        (bubble-up)
+        ; 分离最后一个元素，递归排序前面的元素
+        (#(concat
+            (bubble-sort (drop-last %))
+            [(last %)]
+            ))))))
 
 ; 单元测试
 (let [xsort bubble-sort] (doseq
