15cd2d025fe605a5c64952c463afb1b6771efd55
hanyong <313982441@qq.com>
2013-06-05 01:14:19 +0800

    eval-to-self.clj 推导出一种正确的解

--- src/eval-to-self.clj
+++ src/eval-to-self.clj
@@ -1,13 +1,20 @@
 ;; http://www.4clojure.com/problem/125
 
-(let [x
-         (fn [] 
-           (let [
-                 prefix "(fn [] (let"
-                 suffix "(str prefix ['prefix prefix 'suffix suffix] suffix)))"
-                 ]
-             (str prefix ['prefix prefix 'suffix suffix] suffix)
-             ))
+(let
+  [x
+   '(fn []
+      (let
+        [
+         prefix "(fn [] (let "
+         suffix "))"
+         body '(str prefix ['prefix prefix 'suffix suffix 'body (list 'quote body)] " " body suffix)
          ]
-     (println (x))
-     )
+        ; (eval body) ; CompilerException java.lang.RuntimeException: Unable to resolve symbol: prefix in this context, WHY?
+        (str prefix ['prefix prefix 'suffix suffix 'body (list 'quote body)] " " body suffix)
+        ))
+   ;x (read-string (str x))
+   ]
+  (println (str x))
+  (println ((eval x)))
+  (= (str x) ((eval x)))
+  )
